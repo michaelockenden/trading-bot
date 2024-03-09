2db680dfc86fc6af9425cc4dce9665554dbfc919
michaelockenden <michael.ockenden@hotmail.com>
2024-03-09 03:51:20 +0000

    env management

--- trading_bot/utils/file_utils.py
+++ trading_bot/utils/file_utils.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 
+from environs import Env
+
 
 def project_root() -> Path:
     return Path(__file__).resolve().parent.parent.parent
+
+
+def get_env():
+    env = Env()
+    return env.read_env()
