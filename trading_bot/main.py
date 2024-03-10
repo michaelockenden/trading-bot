a8a841eeab260136643149b66a5794362b2185e7
Jared Schoel <jared.phantom@gmail.com>
2024-03-10 18:49:12 +0000

    refactor

--- trading_bot/main.py
+++ trading_bot/main.py
@@ -8,5 +8,8 @@ if __name__ == "__main__":
     tickers = [Ticker("BTCUSDT"), Ticker("ETHUSDT"), Ticker("SOLUSDT")]
     interval = Interval.ONE_MINUTE
     b_client = BinanceClient(tickers, interval)
-    time.sleep(30)
-    b_client.market_data_provider.stop()
+    provider = b_client.market_data_provider
+    ticker = provider.get_ticker("BTCUSDT")
+    ticker.wait_for_data(30)
+    print(ticker.latest_market_data.close_price)
+    provider.stop()
