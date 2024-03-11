e907fee25c73c91ff6f1a62fe51553a286b0484e
JoelShinu <joelshinu@yahoo.com>
2024-03-11 21:56:17 +0000

    backtesting client

---
+++ trading_bot/market_data/backtesting_client.py
@@ -0,0 +1,41 @@
+import time
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import threading
+import csv
+
+
+class BacktestingEnvironment:
+    def __init__(self, csv_file):
+        self.app = Flask(__name__)
+        self.socketio = SocketIO(self.app)
+        self.csv_file = csv_file
+
+        @self.app.route('/')
+        def index():
+            return render_template('index.html')
+
+        @self.socketio.on('connect')
+        def handle_connect():
+            threading.Thread(target=self.stream_data).start()
+
+    def stream_data(self):
+        with open(self.csv_file, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                timestamp = row[0]
+                data = {
+                    'timestamp': timestamp,
+                    'price': float(row[4])
+                }
+                self.socketio.emit('data', data)
+                time.sleep(1)
+
+    def run(self, host='localhost', port=5000):
+        self.socketio.run(self.app, host=host, port=port, allow_unsafe_werkzeug=True)
+
+
+if __name__ == '__main__':
+    backtesting_env = BacktestingEnvironment('csv.csv')
+    backtesting_env.run()
